5a1d642a70a1e0ee99bca6d89cc49bd08a73e03f
i.samoilenko <i.samoilenko@corp.nekki.ru>
2016-10-24 00:28:21 +0300

    fixes

--- main.py
+++ main.py
@@ -12,20 +12,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return '''Hello from Flask!
-<a href="/projects/">PROJECTS</a>
-<a href="/about/">ABOUT</a>'''
-
-@app.route('/projects/')
-def projects():
-    f = open('index.html', encoding='utf8').read()
+def index():
+    f = open('static/index.html', encoding='utf8').read()
     return f
 
 @app.route('/about')
 def about():
     return 'The about page'
-
 
 
 if __name__ == "__main__":
